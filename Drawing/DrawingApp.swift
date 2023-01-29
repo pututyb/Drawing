dd15d586f3ee07bc59e53ccfd27b13fa12da95c5
Putut Yusri Bahtiar <64458989+pututyb@users.noreply.github.com>
2023-01-29 10:35:32 +0700

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Putut Yusri Bahtiar on 29/01/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
